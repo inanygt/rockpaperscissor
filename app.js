3849048f98c62cf231b4f02076dea494ec0542f1
inan yigit <inanygt@hotmail.com>
2022-07-16 19:17:55 +0200

    Created if else function to play round against computer

--- app.js
+++ app.js
@@ -4,10 +4,32 @@
 // assign number to one of the three variable
 // return variable
 
+// Create a function that will play a round of the game
+// The function should have 2 parameters, you and the computer
+// 
+
+
 function getComputerChoice() {
    let game = ["Rock", "Paper", "Scissors" ]
    let i = Math.floor(Math.random() * 3);
    return game[i];
 }
 
-console.log(getComputerChoice())
+
+function PlayTheGame(playerSelection, computerSelection) {
+   if (playerSelection == "Rock" && computerSelection == "Scissors") {
+      return "You win! Rock beats Scissors";
+   } else if (playerSelection == "Rock" && computerSelection == "Paper" ) {
+      return "You lose! Paper beats Rock";
+   } else if (playerSelection == "paper" && computerSelection == "rock") {
+      return "You win! Paper beats Rock";
+   } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+      return "You lose! Scissors beats Paper";
+   } else if (playerSelection == "Scissors" && computerSelection == "paper") {
+      return "You win! Scissors beats paper";
+   } else {
+      return "You lose! rock beats Scissors"
+   }
+}
+
+console.log(PlayTheGame("Rock" , getComputerChoice()));
