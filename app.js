05d74dd90c74022c341151a48338ab2f08066d39
inan yigit <inanygt@hotmail.com>
2022-07-21 10:10:55 +0200

    Simplified if else statements

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Assign a random number from 0 to 2 to [Rock, Paper, Scissors]
 
 function getComputerChoice() {
    let game = ["Rock", "Paper", "Scissors" ]
@@ -8,6 +9,7 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
+// function that assigns the winner of a round
 
 function playRound(playerSelection, computerSelection) {
    if (playerSelection == computerSelection) {
@@ -21,35 +23,13 @@ function playRound(playerSelection, computerSelection) {
    ){
       playerScore++;
       return "you win!";
-   } if (
-      (playerSelection == "Rock" && computerSelection == "Paper") ||
-      (playerSelection == "Paper" && computerSelection == "Scissors") ||
-      (playerSelection == "Scissors" && computerSelection == "Rock")
-    ) {
+   } else {
          computerScore++;
          return "You lose";
       }
 }
 
-
-// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-
-
-// player starts with 0 points
-// Computer starts with 0 points
-// ——————
-// create a game function
-// Play 5 rounds
-// After every round
-//    If player wins add one point to player
-//    If it is a tie add 1 point to player & computer
-//    If computer wins add 1 point to computer
-// Repeat 5 times 
-// The player with the most points wins 
-
-
-PlayerScore = 0
-ComputerScore = 0
+// Function to play 5 rounds and keep score and give the winner at the end
 
 function game() {
       for (let i = 0; i < 5; i++) {
@@ -58,8 +38,10 @@ function game() {
       }
       if (playerScore > computerScore) {
          return "You win the game!"
-      } else {
+      } else if (playerScore < computerScore) {
          return "You lost the game"
+      } else {
+         return "it's a tie! "
       }
    }
 
